c6ef06b57cd60d8f9b8e2a16d6f4f57575e18fb9
Maciej <255620@edu.p.lodz.pl>
2026-03-31 13:20:27 +0200

    17. Wygenerowanie i uzupełnienie dokumentacji Javadoc

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa reprezentująca domyślną wyliczankę opartą na tablicy.
+ */
 public class DefaultCountingOutRhymer {
 	
 	private static final int NUMBERS_SIZE = 12;
@@ -17,15 +20,31 @@ private static final int EMPTY_RHYMER_INDICATOR = -1;
 	public int getTotal() {
 		return total;
 	}
+
+    /**
+     * Dodaje nową wartość do wyliczanki, jeśli nie jest ona pełna.
+     *
+     * @param in wartość do dodania do wyliczanki
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Sprawdza, czy wyliczanka jest pusta.
+     *
+     * @return true, jeśli wyliczanka jest pusta, false w przeciwnym razie
+     */
     public boolean callCheck() {
         return total == EMPTY_RHYMER_INDICATOR;
     }
 
+    /**
+     * Sprawdza, czy wyliczanka jest pełna.
+     *
+     * @return true, jeśli wyliczanka osiągnęła maksymalną pojemność, false w przeciwnym razie
+     */
     public boolean isFull() {
         return total == FULL_SIZE;
     }
@@ -36,6 +55,12 @@ private static final int EMPTY_RHYMER_INDICATOR = -1;
         return numbers[total];
     }
 
+    /**
+     * Zwraca i usuwa ostatnio dodaną wartość z wyliczanki.
+     *
+     * @return ostatnia dodana wartość lub wskaźnik pustej wyliczanki, jeśli brak elementów
+     *     
+     */
     public int countOut() {
         if (callCheck())
             return EMPTY_RHYMER_INDICATOR;
